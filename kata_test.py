89ee1f8547cbd399e6f01f1db3a6df240ae22218
Camilo Ortiz <ca.ortiz1633@uniandes.edu.co>
2019-10-01 10:29:59 -0500

    Cycle 2: One number test failed

--- kata_test.py
+++ kata_test.py
@@ -3,5 +3,8 @@ from unittest import TestCase
 from kata import Kata
 
 class KataTest(TestCase):
-    def test_solver_function(self):
+    def test_solver_function_empty_string(self):
         self.assertEqual(Kata().solver_function(""), [], "Cycle 1: Empty String")
+
+    def test_solver_function_one_number(self):
+        self.assertEqual(Kata().solver_function("1"), [1], "Cycle 2: One Number")
