18285136a95e81fdf6d3eecd506999a10dcd0db9
Camilo Ortiz <ca.ortiz1633@uniandes.edu.co>
2019-10-01 10:37:34 -0500

    Cycle 2: One number test passed

--- kata.py
+++ kata.py
@@ -2,3 +2,7 @@ class Kata:
     def solver_function(self, numbers):
         if numbers == "":
             return []
+        else:
+            res = []
+            res += [int(numbers)]
+            return res
